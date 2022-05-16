0820ae180d4e2d4049743d75923c2d01355c3f67
Wilson Oliveira <wilson.woliveira@gmail.com>
2022-05-16 01:08:35 -0300

    Atualizando configuracoes e adicionando tema principal

--- ecommerce/src/wp-config.php
+++ ecommerce/src/wp-config.php
@@ -83,6 +83,7 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 define( 'WP_DEBUG', false );
+define('WP_POST_REVISIONS', false);
 
 /* Adicione valores personalizados entre esta linha até "Isto é tudo". */
 
