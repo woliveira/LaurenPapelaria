c73deef96474402fffbcda1aef3128347d914144
Wilson Oliveira <wilson.woliveira@gmail.com>
2022-05-19 00:55:56 -0300

    limpando o ambiente

--- ecommerce/src/wp-content/plugins/woocommerce-mercadopago/assets/js/review.js
+++
@@ -1,7 +0,0 @@
-/* globals ajaxurl */
-jQuery(document).ready(function ($) {
-  $(document).on('click', '.mp-rating-notice button', function () {
-      $.post( ajaxurl, { action: 'mercadopago_review_dismiss' } );
-    }
-  );
-});
