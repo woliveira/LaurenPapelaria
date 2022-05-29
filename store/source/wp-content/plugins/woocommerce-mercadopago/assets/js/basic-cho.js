79223fb7e8e6f168730516e51cac5e0a9279dbba
Wilson Oliveira <wilson.woliveira@gmail.com>
2022-05-29 17:58:23 -0300

    Removendo todos os arquivos

--- store/source/wp-content/plugins/woocommerce-mercadopago/assets/js/basic-cho.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  if (document.getElementById('payment') !== undefined && document.getElementById('payment').offsetWidth <= 530) {
-    var framePayments = document.querySelectorAll('#framePayments');
-    for (var i = 0; i < framePayments.length; i++) {
-      framePayments[i].className = '';
-      framePayments[i].classList.add('mp-col-md-12');
-    }
-  }
-})();
